fb56cce2a3a9c21b2f413b87b50da82115b03731
vdefino <verdiana.de-fino@irit.fr>
2017-04-12 16:24:27 +0200

    essai

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int adition (int x, int y)
+	return x + y;
+
+int soustraction (int x, int y)
+	return x - y;
+
+int mutliplication (int x, int y)
+	return x * y;
+
+int main(){
+	
+	printf("adition = %d, soustraction = %d, multiplication = %d\n", adition(2, 2), soustraction(2, 2), multiplication(2, 2));
+
+return 0;
